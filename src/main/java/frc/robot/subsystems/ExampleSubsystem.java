229255cef84e1fc7f8e0382bd0c823903502bd96
Satvik Sivaraman <satsiv2007@gmail.com>
2023-10-19 17:57:50 -0700

    Added sensor readings

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -4,74 +4,106 @@
 
 package frc.robot.subsystems;
 
+// Imports
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ColorSensorV3;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class ExampleSubsystem extends SubsystemBase {
 
-  private final CANSparkMax main;
-  private final CANSparkMax follower;
+  // Motors
+  private final CANSparkMax topMotor;
+  private final CANSparkMax bottomMotor;
+  // Controller + buttons
   private final XboxController controller;
   private final JoystickButton buttonA;
   private final JoystickButton buttonB;
   private final JoystickButton buttonX;
   private final JoystickButton buttonY;
+  // Sensors
+  private final ColorSensorV3 color;
+  private final AnalogPotentiometer topIR;
+  private final AnalogPotentiometer bottomIR;
+  // Timer
+  private final Timer timer;
+
+  // Motor variables
+  private double topSpeed = 0;
+  private double bottomSpeed = 0;
+  // Button status variables
   private boolean aPress;
   private boolean bPress;
   private boolean xPress;
   private boolean yPress;
-  private final Timer timer;
+  // Distance variables
+  private double topDist = 0;
+  private double bottomDist = 0;
+  // Color sensor variables
+  private Color colorSeen = Color.kBlack;
+  // Timer variables
   private boolean started = false;
-  private double speed = 0;
-  private double multi = 1;
+  // Status variables
+  private int balls = 0;
 
   public ExampleSubsystem() {
-    main = new CANSparkMax(1, MotorType.kBrushless);
-    follower = new CANSparkMax(2, MotorType.kBrushless);
-    follower.follow(main);
+    // Define motors
+    topMotor = new CANSparkMax(13, MotorType.kBrushless);
+    bottomMotor = new CANSparkMax(14, MotorType.kBrushless);
+    // Define controller + buttons
     controller = new XboxController(0);
     buttonA = new JoystickButton(controller, XboxController.Button.kA.value);
     buttonB = new JoystickButton(controller, XboxController.Button.kB.value);
     buttonX = new JoystickButton(controller, XboxController.Button.kX.value);
     buttonY = new JoystickButton(controller, XboxController.Button.kY.value);
+    // Define sensors
+    color = new ColorSensorV3(I2C.Port.kMXP);
+    topIR = new AnalogPotentiometer(1);
+    bottomIR = new AnalogPotentiometer(0);
+    // Define timer
     timer = new Timer();
   }
 
+  // Ignore, everything breaks without this :)
   public CommandBase exampleMethodCommand() {return runOnce(() -> {});}
   public boolean exampleCondition() {return false;}
 
-  @Override
-  public void periodic() {
+  public void updateButtons() {
+    // Update button status variables
     aPress = buttonA.getAsBoolean();
     bPress = buttonB.getAsBoolean();
     xPress = buttonX.getAsBoolean();
     yPress = buttonY.getAsBoolean();
+  }
 
-    if (aPress && !bPress) {
-      speed = 0.25;
-    } else if (!aPress && bPress) {
-      speed = -0.25;
-    } else {
-      speed = 0;
-    }
+  public void updateSensors() {
+    // Update sensor variables
+    topDist = topIR.get();
+    bottomDist = bottomIR.get();
+    colorSeen = color.getColor();
+  }
 
-    if (!started && (xPress || yPress)) {
-      started = true;
-      timer.start();
-      if (xPress) multi = 2;
-      else if (yPress) multi = 0.5;
-    } else if (started && timer.hasElapsed(2)) {
-      timer.stop();
-      timer.reset();
-      started = false;
-      multi = 1;
-    }
+  public void updateBalls() {
+    // Update number of balls in intake
+    // TODO
+  }
 
-    main.set(speed*multi);
+  @Override
+  public void periodic() {
+    // Update variables
+    updateButtons();
+    updateSensors();
+    updateBalls();
+
+    // TODO
+    topMotor.set(topSpeed);
+    bottomMotor.set(bottomSpeed);
   }
 }
